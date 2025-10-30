7090aa304cf58ded969af15bcebb73cc02ef6ece
shahzada-shah <134459237+shahzada-shah@users.noreply.github.com>
2025-10-29 22:24:35 -0400

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Hero from '../components/layout/Hero'
 import { ValueProps } from '../components/sections/ValueProps'
 import { Bestsellers } from '../components/sections/Bestsellers'
@@ -5,8 +6,25 @@ import { ShopByActivity } from '../components/sections/ShopByActivity'
 import { MantraSection } from '../components/sections/MantraSection'
 import { BrandBanner } from '../components/sections/BrandBanner'
 import { BlogSection } from '../components/sections/BlogSection'
+import { NewsletterModal } from '../components/ui/NewsletterModal'
+
+const MODAL_DELAY = 3000
 
 export const Home = () => {
+  const [isNewsletterOpen, setIsNewsletterOpen] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsNewsletterOpen(true)
+    }, MODAL_DELAY)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  const handleNewsletterClose = () => {
+    setIsNewsletterOpen(false)
+  }
+
   return (
     <>
       <Hero />
@@ -28,6 +46,8 @@ export const Home = () => {
       <BlogSection />
 
       <MantraSection />
+
+      <NewsletterModal isOpen={isNewsletterOpen} onClose={handleNewsletterClose} />
     </>
   )
 }
